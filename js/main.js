3d23cf742f1761e5276380df3a66b41e1ee641e9
pgm-meesakveld <mees.akveld@student.arteveldehs.be>
2024-04-18 16:45:12 +0200

    Refactored button position logic in main.js

--- js/main.js
+++ js/main.js
@@ -5,11 +5,8 @@ const height = window.innerHeight;
 console.log(`ClientHeight: ${height} - ClientWidth: ${width}`);
 
 btn.addEventListener("click", () => {
-    let randValueX = Math.floor(((Math.random() * width) + 1) / 2);
-    let randValueY = Math.floor(((Math.random() * height) + 1)/ 2);
-    //console.log(`Random width: ${randValueX}`);
-    //console.log(`Random height: ${randValueY}`);
-    //btn.style.top = randValueY + 'px';
-    //btn.style.right = randValueX + 'px';
-    console.log(`ClientHeight: ${height} - ClientWidth: ${width}`);
+    let randValueX = (Math.floor(((Math.random() * width) + 1) / 2) - 150);
+    let randValueY = (Math.floor(((Math.random() * height) + 1)/ 2) - 150);
+    btn.style.top = randValueY + 'px';
+    btn.style.right = randValueX + 'px';
 });
