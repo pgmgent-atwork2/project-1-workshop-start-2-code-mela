3427848e8580a58ca0d4aee95015ed72261a38a3
pgm-meesakveld <mees.akveld@student.arteveldehs.be>
2024-04-19 10:41:06 +0200

    Separated questions file

---
+++ js/questions.js
@@ -0,0 +1,52 @@
+const questions = [
+    {
+        "question": "Wat is de hoofdstad van Frankrijk?",
+        "correctAnswer": "Parijs",
+        "wrongAnswer": "Rome"
+    },
+    {
+        "question": "Wie schilderde de Mona Lisa?",
+        "correctAnswer": "Leonardo da Vinci",
+        "wrongAnswer": "Vincent van Gogh"
+    },
+    {
+        "question": "Hoeveel planeten zijn er in ons zonnestelsel?",
+        "correctAnswer": "8",
+        "wrongAnswer": "9"
+    },
+    {
+        "question": "Wat is de langste rivier ter wereld?",
+        "correctAnswer": "De Nijl",
+        "wrongAnswer": "De Amazone"
+    },
+    {
+        "question": "Wie schreef 'Romeo en Julia'?",
+        "correctAnswer": "William Shakespeare",
+        "wrongAnswer": "Charles Dickens"
+    },
+    {
+        "question": "Wat is de hoofdstad van Spanje?",
+        "correctAnswer": "Madrid",
+        "wrongAnswer": "Barcelona"
+    },
+    {
+        "question": "Hoeveel continenten zijn er op aarde?",
+        "correctAnswer": "7",
+        "wrongAnswer": "6"
+    },
+    {
+        "question": "Wie was de eerste mens op de maan?",
+        "correctAnswer": "Neil Armstrong",
+        "wrongAnswer": "Buzz Aldrin"
+    },
+    {
+        "question": "Wat is de meest gesproken taal ter wereld?",
+        "correctAnswer": "Mandarijn (Chinees)",
+        "wrongAnswer": "Engels"
+    },
+    {
+        "question": "Wie schreef '1984'?",
+        "correctAnswer": "George Orwell",
+        "wrongAnswer": "Aldous Huxley"
+    }
+]
